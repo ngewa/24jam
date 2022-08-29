aea6ca8819dbd7278f63345fca0b04e60e5e0e55
ngewa <admonfast3@gmail.com>
2022-08-29 15:42:44 +0700

    ngewa doang bang

--- index.js
+++ index.js
@@ -13,10 +13,12 @@ var moveinterval = 2; // 2 second movement interval
 var maxrandom = 5; // 0-5 seconds added to movement interval (randomly)
 var host = data["ip"];
 var username = data["name"]
+var port = data["port"]
 var nightskip = data["auto-night-skip"]
 var bot = mineflayer.createBot({
   host: host,
-  username: username
+  username: username,
+  port: port
 });
 function getRandomArbitrary(min, max) {
        return Math.random() * (max - min) + min;
